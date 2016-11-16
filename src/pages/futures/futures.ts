0d6969cea5768dbec84a72ebbf88ab87612005b7
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2016-11-16 14:51:36 +0800

    update

--- src/pages/futures/futures.ts
+++ src/pages/futures/futures.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform, NavController } from 'ionic-angular';
-import { InAppBrowser } from 'ionic-native';
+import { SecureStorage, InAppBrowser } from 'ionic-native';
 
 /*
   Generated class for the Futures page.
@@ -23,6 +23,17 @@ export class FuturesPage {
       let browser = new InAppBrowser('http://futures.shtx.com.cn/', '_blank');
       browser.show();
     });
+  }
+
+  clear(){
+    let secureStorage: SecureStorage = new SecureStorage();
+    secureStorage.create('tongxin')
+      .then(
+      () => console.log('Storage is ready!'),
+      error => {
+      });
+      secureStorage.remove('mobile').then();
+      secureStorage.remove('password').then();
   }
 
 }
