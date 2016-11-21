ec7ace754237b3472ebb1cd7d95be34d00e2d1be
郭轩 <guoxuan@guoxuan.local>
2016-11-21 11:47:27 +0800

    OK

--- src/pages/price-chart/price-chart.ts
+++ src/pages/price-chart/price-chart.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+declare const c3: any;
 
 /*
   Generated class for the PriceChart page.
@@ -12,10 +13,24 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'price-chart.html'
 })
 export class PriceChartPage {
-  
+
   product: any;
+  items = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.product = navParams.get('product');
+    this.items = navParams.get('items');
+  }
+
+  ionViewWillEnter() {
+    var chart = c3.generate({
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25]
+        ]
+      }
+    });
   }
 
 }
