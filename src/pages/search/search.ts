e8b4ca70824590901813fcb84defa9a580def6fc
郭轩 <guoxuan@guoxuan.local>
2016-12-08 16:57:45 +0800

    OK

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, Input } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, ViewController } from 'ionic-angular';
 
 /*
