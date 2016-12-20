7fd098aa340f068b8bdb88ca361748069e324e7f
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2016-12-20 16:06:24 +0800

    onboard button and search comment

--- src/providers/search-svc.ts
+++ src/providers/search-svc.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 import * as Promise from 'promise';
 import { Global } from '../providers/global';
@@ -30,13 +30,26 @@ export class SearchSvc {
 
   getPLSearchData(mobile, searchKey){
     return new Promise((resolve, reject) => {
-      this.http.get(this.global.SERVER + "/Handlers/PLHandler.ashx?method=search&mobile=" + mobile + "&key=" + searchKey).map(res => res.json())
+      let body = this.jsonToURLEncoded({ method: 'search', mobile: mobile, key: searchKey });
+      let headers = new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      });
+      let options = new RequestOptions({
+        headers: headers
+      });
+      this.http.post(this.global.SERVER + "/Handlers/PLHandler.ashx", body, options).map(res => res.json())
       .subscribe((data) => {
         resolve(data);
       },err => {
         throw new Error(err);
       })
     })
+  }
+
+  jsonToURLEncoded(jsonString){
+    return Object.keys(jsonString).map(function(key){
+      return encodeURIComponent(key) + '=' + encodeURIComponent(jsonString[key]);
+    }).join('&');
   }
 
 }
